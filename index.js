0d16b298bb82cf3c56c8067d316f8dd6953b710c
jhsware <sebastian@urbantalk.se>
2017-09-13 15:07:42 +0200

    Missing entry point

---
+++ index.js
@@ -0,0 +1,7 @@
+module.exports = {
+  Manager: require('./dist/Manager'),
+  Target: require('./dist/Target'),
+  Popper: require('./dist/Popper'),
+  Arrow: require('./dist/Arrow'),
+  Travel: require('./dist/Travel')
+} 
